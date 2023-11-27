eed8f53a6708c0ddcd17488b03d71bad6438eb3c
Ahmad-ELHalabi <152052931+Ahmad-ELHalabi@users.noreply.github.com>
2023-11-27 13:24:26 +0200

    Adding radio buttons and checkbox button

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,15 +1,18 @@
-// import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:project1/models/item_model.dart';
+import 'package:realstateflutter/models/item_model.dart';
 
 class DetailsScreen extends StatefulWidget {
   DetailsScreen(this.item, {Key? key}) : super(key: key);
-  Item item;
+  final Item item;
+
   @override
   State<DetailsScreen> createState() => _DetailsScreenState();
 }
 
 class _DetailsScreenState extends State<DetailsScreen> {
+  bool isFavorite = false; // Added variable to track the checkbox state
+  int rating = -1; // Added variable to track the selected rating
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,8 +39,9 @@ class _DetailsScreenState extends State<DetailsScreen> {
                 color: Colors.grey.shade400,
                 borderRadius: BorderRadius.circular(10.0),
                 image: DecorationImage(
-                    image: NetworkImage(widget.item.thumb_url!),
-                    fit: BoxFit.cover),
+                  image: NetworkImage(widget.item.thumb_url!),
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
             SizedBox(
@@ -93,32 +97,114 @@ class _DetailsScreenState extends State<DetailsScreen> {
                   style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22.0),
                   overflow: TextOverflow.ellipsis,
                 ),
-                // IconButton(
-                //   onPressed: () {},
-                //   icon: Icon(Icons.favorite_border_outlined),
-                // ),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.favorite_outline),
+                Row(
+                  children: [
+                    Checkbox(
+                      value: isFavorite,
+                      onChanged: (value) {
+                        setState(() {
+                          isFavorite = value!;
+                        });
+                      },
+                    ),
+                    Text(
+                      'Add to Favorites', // Your meaningful text label
+                      style: TextStyle(
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              'Please rate this state on a scale from very bad to very good.',
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                RadioListTile<int>(
+                  title: Text('Very Bad'),
+                  value: 0,
+                  groupValue: rating,
+                  onChanged: (value) {
+                    setState(() {
+                      rating = value!;
+                    });
+                  },
+                ),
+                RadioListTile<int>(
+                  title: Text('Bad'),
+                  value: 1,
+                  groupValue: rating,
+                  onChanged: (value) {
+                    setState(() {
+                      rating = value!;
+                    });
+                  },
+                ),
+                RadioListTile<int>(
+                  title: Text('Fine'),
+                  value: 2,
+                  groupValue: rating,
+                  onChanged: (value) {
+                    setState(() {
+                      rating = value!;
+                    });
+                  },
+                ),
+                RadioListTile<int>(
+                  title: Text('Good'),
+                  value: 3,
+                  groupValue: rating,
+                  onChanged: (value) {
+                    setState(() {
+                      rating = value!;
+                    });
+                  },
+                ),
+                RadioListTile<int>(
+                  title: Text('Very Good'),
+                  value: 4,
+                  groupValue: rating,
+                  onChanged: (value) {
+                    setState(() {
+                      rating = value!;
+                    });
+                  },
                 ),
               ],
             ),
             Expanded(child: Container()),
             Container(
-                width: double.infinity,
-                height: 60.0,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(16.0),
+              width: double.infinity,
+              height: 60.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16.0),
+              ),
+              child: RawMaterialButton(
+                onPressed: () {},
+                elevation: 0.0,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(6),
                 ),
-                child: RawMaterialButton(
-                    onPressed: () {},
-                    elevation: 0.0,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(6)),
-                    fillColor: Colors.blue.shade600,
-                    child: Text("Rent Now",
-                        style:
-                            TextStyle(color: Colors.white, fontSize: 18.0)))),
+                fillColor: Colors.blue.shade600,
+                child: Text(
+                  "Rent Now",
+                  style: TextStyle(color: Colors.white, fontSize: 18.0),
+                ),
+              ),
+            ),
           ],
         ),
       ),
